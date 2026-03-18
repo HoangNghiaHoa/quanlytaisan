f4453853fff374136c3d21ef2fbfa49edb85d84b
Hoang Nghia Hoa <hoabinh123thpt@gmail.com>
2026-03-18 15:43:48 +0700

    change URL

--- FE/kind-site-engine/src/services/api.ts
+++ FE/kind-site-engine/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api";
+const API_BASE_URL = "https://quanlytaisan-m2oc.onrender.com";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
